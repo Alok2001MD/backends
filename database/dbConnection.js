bf08d7a58d44be30249981ed19701e6c7eb2a75d
Alok2001MD <alokmattihalli@gmail.com>
2024-04-26 14:22:59 +0530

    hello

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const dbConnection = () => {
   mongoose
-    .connect(process.env.MONGO_URI, {
+    .connect("mongodb+srv://codewithalok:codewithalok@cluster0.qwzzumq.mongodb.net/?retryWrites=true", {
       dbName: "MERN_JOB_SEEKING_WEBAPP",
     })
     .then(() => {
--- server.js
+++ server.js
@@ -2,11 +2,11 @@ import app from "./app.js";
 import cloudinary from "cloudinary";
 
 cloudinary.v2.config({
-  cloud_name: process.env.CLOUDINARY_CLIENT_NAME,
-  api_key: process.env.CLOUDINARY_CLIENT_API,
-  api_secret: process.env.CLOUDINARY_CLIENT_SECRET,
+  cloud_name: duzqrxe8t,
+  api_key: 134755747279345,
+  api_secret: VIuVW7RTt8hTSIXwohMbyzA48Qo,
 });
 
-app.listen(process.env.PORT||10000, () => {
-  console.log(`Server running at port ${process.env.PORT}`);
+app.listen(4000||10000, () => {
+  console.log(`Server running at port 4000`);
 });
