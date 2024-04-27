fae431090bff071b1dbcc5ce3da5e8da87b08a39
Alok2001MD <alokmattihalli@gmail.com>
2024-04-27 12:09:56 +0530

    hellll

--- app.js
+++ app.js
@@ -30,9 +30,7 @@ app.use(
     tempFileDir: "/tmp/",
   })
 );
-// app.use('/',(req,res)=>{
-//   res.send("Hello");
-// })
+
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/job", jobRouter);
 app.use("/api/v1/application", applicationRouter);
